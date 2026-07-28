5158f8e4e779114a8e19e780eebe90dfe8b6eb84
AmaiOmikan <64887878+kg9n3n8y@users.noreply.github.com>
2026-07-28 21:39:45 +0900

    札の情報を追加

---
+++ fudalist.js
@@ -0,0 +1,702 @@
+const fudalist = [
+    {
+        no: 1,
+        kimariji: "あきの",
+        normal: "./torifuda/tori_1.png",
+        reverse: "./torifuda/tori_r_1.png",
+        order: 89
+    },
+    {
+        no: 2,
+        kimariji: "はるす",
+        normal: "./torifuda/tori_2.png",
+        reverse: "./torifuda/tori_r_2.png",
+        order: 30
+    },
+    {
+        no: 3,
+        kimariji: "あし",
+        normal: "./torifuda/tori_3.png",
+        reverse: "./torifuda/tori_r_3.png",
+        order: 98
+    },
+    {
+        no: 4,
+        kimariji: "たご",
+        normal: "./torifuda/tori_4.png",
+        reverse: "./torifuda/tori_r_4.png",
+        order: 53
+    },
+    {
+        no: 5,
+        kimariji: "おく",
+        normal: "./torifuda/tori_5.png",
+        reverse: "./torifuda/tori_r_5.png",
+        order: 63
+    },
+    {
+        no: 6,
+        kimariji: "かさ",
+        normal: "./torifuda/tori_6.png",
+        reverse: "./torifuda/tori_r_6.png",
+        order: 43
+    },
+    {
+        no: 7,
+        kimariji: "あまの",
+        normal: "./torifuda/tori_7.png",
+        reverse: "./torifuda/tori_r_7.png",
+        order: 92
+    },
+    {
+        no: 8,
+        kimariji: "わがい",
+        normal: "./torifuda/tori_8.png",
+        reverse: "./torifuda/tori_r_8.png",
+        order: 71
+    },
+    {
+        no: 9,
+        kimariji: "はなの",
+        normal: "./torifuda/tori_9.png",
+        reverse: "./torifuda/tori_r_9.png",
+        order: 33
+    },
+    {
+        no: 10,
+        kimariji: "これ",
+        normal: "./torifuda/tori_10.png",
+        reverse: "./torifuda/tori_r_10.png",
+        order: 57
+    },
+    {
+        no: 11,
+        kimariji: "わたのはらや",
+        normal: "./torifuda/tori_11.png",
+        reverse: "./torifuda/tori_r_11.png",
+        order: 75
+    },
+    {
+        no: 12,
+        kimariji: "あまつ",
+        normal: "./torifuda/tori_12.png",
+        reverse: "./torifuda/tori_r_12.png",
+        order: 91
+    },
+    {
+        no: 13,
+        kimariji: "つく",
+        normal: "./torifuda/tori_13.png",
+        reverse: "./torifuda/tori_r_13.png",
+        order: 11
+    },
+    {
+        no: 14,
+        kimariji: "みち",
+        normal: "./torifuda/tori_14.png",
+        reverse: "./torifuda/tori_r_14.png",
+        order: 46
+    },
+    {
+        no: 15,
+        kimariji: "きみがためは",
+        normal: "./torifuda/tori_15.png",
+        reverse: "./torifuda/tori_r_15.png",
+        order: 28
+    },
+    {
+        no: 16,
+        kimariji: "たち",
+        normal: "./torifuda/tori_16.png",
+        reverse: "./torifuda/tori_r_16.png",
+        order: 52
+    },
+    {
+        no: 17,
+        kimariji: "ちは",
+        normal: "./torifuda/tori_17.png",
+        reverse: "./torifuda/tori_r_17.png",
+        order: 21
+    },
+    {
+        no: 18,
+        kimariji: "す",
+        normal: "./torifuda/tori_18.png",
+        reverse: "./torifuda/tori_r_18.png",
+        order: 2
+    },
+    {
+        no: 19,
+        kimariji: "なにわが",
+        normal: "./torifuda/tori_19.png",
+        reverse: "./torifuda/tori_r_19.png",
+        order: 83
+    },
+    {
+        no: 20,
+        kimariji: "わび",
+        normal: "./torifuda/tori_20.png",
+        reverse: "./torifuda/tori_r_20.png",
+        order: 70
+    },
+    {
+        no: 21,
+        kimariji: "いまこ",
+        normal: "./torifuda/tori_21.png",
+        reverse: "./torifuda/tori_r_21.png",
+        order: 20
+    },
+    {
+        no: 22,
+        kimariji: "ふ",
+        normal: "./torifuda/tori_22.png",
+        reverse: "./torifuda/tori_r_22.png",
+        order: 4
+    },
+    {
+        no: 23,
+        kimariji: "つき",
+        normal: "./torifuda/tori_23.png",
+        reverse: "./torifuda/tori_r_23.png",
+        order: 10
+    },
+    {
+        no: 24,
+        kimariji: "この",
+        normal: "./torifuda/tori_24.png",
+        reverse: "./torifuda/tori_r_24.png",
+        order: 59
+    },
+    {
+        no: 25,
+        kimariji: "なにし",
+        normal: "./torifuda/tori_25.png",
+        reverse: "./torifuda/tori_r_25.png",
+        order: 82
+    },
+    {
+        no: 26,
+        kimariji: "おぐ",
+        normal: "./torifuda/tori_26.png",
+        reverse: "./torifuda/tori_r_26.png",
+        order: 66
+    },
+    {
+        no: 27,
+        kimariji: "みかの",
+        normal: "./torifuda/tori_27.png",
+        reverse: "./torifuda/tori_r_27.png",
+        order: 50
+    },
+    {
+        no: 28,
+        kimariji: "やまざ",
+        normal: "./torifuda/tori_28.png",
+        reverse: "./torifuda/tori_r_28.png",
+        order: 37
+    },
+    {
+        no: 29,
+        kimariji: "こころあ",
+        normal: "./torifuda/tori_29.png",
+        reverse: "./torifuda/tori_r_29.png",
+        order: 61
+    },
+    {
+        no: 30,
+        kimariji: "ありあ",
+        normal: "./torifuda/tori_30.png",
+        reverse: "./torifuda/tori_r_30.png",
+        order: 94
+    },
+    {
+        no: 31,
+        kimariji: "あさぼらけあ",
+        normal: "./torifuda/tori_31.png",
+        reverse: "./torifuda/tori_r_31.png",
+        order: 96
+    },
+    {
+        no: 32,
+        kimariji: "やまが",
+        normal: "./torifuda/tori_32.png",
+        reverse: "./torifuda/tori_r_32.png",
+        order: 36
+    },
+    {
+        no: 33,
+        kimariji: "ひさ",
+        normal: "./torifuda/tori_33.png",
+        reverse: "./torifuda/tori_r_33.png",
+        order: 24
+    },
+    {
+        no: 34,
+        kimariji: "たれ",
+        normal: "./torifuda/tori_34.png",
+        reverse: "./torifuda/tori_r_34.png",
+        order: 56
+    },
+    {
+        no: 35,
+        kimariji: "ひとは",
+        normal: "./torifuda/tori_35.png",
+        reverse: "./torifuda/tori_r_35.png",
+        order: 25
+    },
+    {
+        no: 36,
+        kimariji: "なつ",
+        normal: "./torifuda/tori_36.png",
+        reverse: "./torifuda/tori_r_36.png",
+        order: 77
+    },
+    {
+        no: 37,
+        kimariji: "しら",
+        normal: "./torifuda/tori_37.png",
+        reverse: "./torifuda/tori_r_37.png",
+        order: 12
+    },
+    {
+        no: 38,
+        kimariji: "わすら",
+        normal: "./torifuda/tori_38.png",
+        reverse: "./torifuda/tori_r_38.png",
+        order: 74
+    },
+    {
+        no: 39,
+        kimariji: "あさじ",
+        normal: "./torifuda/tori_39.png",
+        reverse: "./torifuda/tori_r_39.png",
+        order: 95
+    },
+    {
+        no: 40,
+        kimariji: "しの",
+        normal: "./torifuda/tori_40.png",
+        reverse: "./torifuda/tori_r_40.png",
+        order: 13
+    },
+    {
+        no: 41,
+        kimariji: "こい",
+        normal: "./torifuda/tori_41.png",
+        reverse: "./torifuda/tori_r_41.png",
+        order: 58
+    },
+    {
+        no: 42,
+        kimariji: "ちぎりき",
+        normal: "./torifuda/tori_42.png",
+        reverse: "./torifuda/tori_r_42.png",
+        order: 22
+    },
+    {
+        no: 43,
+        kimariji: "あい",
+        normal: "./torifuda/tori_43.png",
+        reverse: "./torifuda/tori_r_43.png",
+        order: 99
+    },
+    {
+        no: 44,
+        kimariji: "おおこ",
+        normal: "./torifuda/tori_44.png",
+        reverse: "./torifuda/tori_r_44.png",
+        order: 69
+    },
+    {
+        no: 45,
+        kimariji: "あわれ",
+        normal: "./torifuda/tori_45.png",
+        reverse: "./torifuda/tori_r_45.png",
+        order: 86
+    },
+    {
+        no: 46,
+        kimariji: "ゆら",
+        normal: "./torifuda/tori_46.png",
+        reverse: "./torifuda/tori_r_46.png",
+        order: 17
+    },
+    {
+        no: 47,
+        kimariji: "やえ",
+        normal: "./torifuda/tori_47.png",
+        reverse: "./torifuda/tori_r_47.png",
+        order: 35
+    },
+    {
+        no: 48,
+        kimariji: "かぜを",
+        normal: "./torifuda/tori_48.png",
+        reverse: "./torifuda/tori_r_48.png",
+        order: 44
+    },
+    {
+        no: 49,
+        kimariji: "みかき",
+        normal: "./torifuda/tori_49.png",
+        reverse: "./torifuda/tori_r_49.png",
+        order: 49
+    },
+    {
+        no: 50,
+        kimariji: "きみがためお",
+        normal: "./torifuda/tori_50.png",
+        reverse: "./torifuda/tori_r_50.png",
+        order: 29
+    },
+    {
+        no: 51,
+        kimariji: "かく",
+        normal: "./torifuda/tori_51.png",
+        reverse: "./torifuda/tori_r_51.png",
+        order: 42
+    },
+    {
+        no: 52,
+        kimariji: "あけ",
+        normal: "./torifuda/tori_52.png",
+        reverse: "./torifuda/tori_r_52.png",
+        order: 100
+    },
+    {
+        no: 53,
+        kimariji: "なげき",
+        normal: "./torifuda/tori_53.png",
+        reverse: "./torifuda/tori_r_53.png",
+        order: 80
+    },
+    {
+        no: 54,
+        kimariji: "わすれ",
+        normal: "./torifuda/tori_54.png",
+        reverse: "./torifuda/tori_r_54.png",
+        order: 73
+    },
+    {
+        no: 55,
+        kimariji: "たき",
+        normal: "./torifuda/tori_55.png",
+        reverse: "./torifuda/tori_r_55.png",
+        order: 55
+    },
+    {
+        no: 56,
+        kimariji: "あらざ",
+        normal: "./torifuda/tori_56.png",
+        reverse: "./torifuda/tori_r_56.png",
+        order: 88
+    },
+    {
+        no: 57,
+        kimariji: "め",
+        normal: "./torifuda/tori_57.png",
+        reverse: "./torifuda/tori_r_57.png",
+        order: 3
+    },
+    {
+        no: 58,
+        kimariji: "ありま",
+        normal: "./torifuda/tori_58.png",
+        reverse: "./torifuda/tori_r_58.png",
+        order: 93
+    },
+    {
+        no: 59,
+        kimariji: "やす",
+        normal: "./torifuda/tori_59.png",
+        reverse: "./torifuda/tori_r_59.png",
+        order: 34
+    },
+    {
+        no: 60,
+        kimariji: "おおえ",
+        normal: "./torifuda/tori_60.png",
+        reverse: "./torifuda/tori_r_60.png",
+        order: 67
+    },
+    {
+        no: 61,
+        kimariji: "いに",
+        normal: "./torifuda/tori_61.png",
+        reverse: "./torifuda/tori_r_61.png",
+        order: 18
+    },
+    {
+        no: 62,
+        kimariji: "よを",
+        normal: "./torifuda/tori_62.png",
+        reverse: "./torifuda/tori_r_62.png",
+        order: 38
+    },
+    {
+        no: 63,
+        kimariji: "いまは",
+        normal: "./torifuda/tori_63.png",
+        reverse: "./torifuda/tori_r_63.png",
+        order: 19
+    },
+    {
+        no: 64,
+        kimariji: "あさぼらけう",
+        normal: "./torifuda/tori_64.png",
+        reverse: "./torifuda/tori_r_64.png",
+        order: 97
+    },
+    {
+        no: 65,
+        kimariji: "うら",
+        normal: "./torifuda/tori_65.png",
+        reverse: "./torifuda/tori_r_65.png",
+        order: 9
+    },
+    {
+        no: 66,
+        kimariji: "もろ",
+        normal: "./torifuda/tori_66.png",
+        reverse: "./torifuda/tori_r_66.png",
+        order: 14
+    },
+    {
+        no: 67,
+        kimariji: "はるの",
+        normal: "./torifuda/tori_67.png",
+        reverse: "./torifuda/tori_r_67.png",
+        order: 31
+    },
+    {
+        no: 68,
+        kimariji: "こころに",
+        normal: "./torifuda/tori_68.png",
+        reverse: "./torifuda/tori_r_68.png",
+        order: 62
+    },
+    {
+        no: 69,
+        kimariji: "あらし",
+        normal: "./torifuda/tori_69.png",
+        reverse: "./torifuda/tori_r_69.png",
+        order: 87
+    },
+    {
+        no: 70,
+        kimariji: "さ",
+        normal: "./torifuda/tori_70.png",
+        reverse: "./torifuda/tori_r_70.png",
+        order: 5
+    },
+    {
+        no: 71,
+        kimariji: "ゆう",
+        normal: "./torifuda/tori_71.png",
+        reverse: "./torifuda/tori_r_71.png",
+        order: 16
+    },
+    {
+        no: 72,
+        kimariji: "おと",
+        normal: "./torifuda/tori_72.png",
+        reverse: "./torifuda/tori_r_72.png",
+        order: 64
+    },
+    {
+        no: 73,
+        kimariji: "たか",
+        normal: "./torifuda/tori_73.png",
+        reverse: "./torifuda/tori_r_73.png",
+        order: 51
+    },
+    {
+        no: 74,
+        kimariji: "うか",
+        normal: "./torifuda/tori_74.png",
+        reverse: "./torifuda/tori_r_74.png",
+        order: 8
+    },
+    {
+        no: 75,
+        kimariji: "ちぎりお",
+        normal: "./torifuda/tori_75.png",
+        reverse: "./torifuda/tori_r_75.png",
+        order: 23
+    },
+    {
+        no: 76,
+        kimariji: "わたのはらこ",
+        normal: "./torifuda/tori_76.png",
+        reverse: "./torifuda/tori_r_76.png",
+        order: 76
+    },
+    {
+        no: 77,
+        kimariji: "せ",
+        normal: "./torifuda/tori_77.png",
+        reverse: "./torifuda/tori_r_77.png",
+        order: 7
+    },
+    {
+        no: 78,
+        kimariji: "あわじ",
+        normal: "./torifuda/tori_78.png",
+        reverse: "./torifuda/tori_r_78.png",
+        order: 85
+    },
+    {
+        no: 79,
+        kimariji: "あきか",
+        normal: "./torifuda/tori_79.png",
+        reverse: "./torifuda/tori_r_79.png",
+        order: 90
+    },
+    {
+        no: 80,
+        kimariji: "ながか",
+        normal: "./torifuda/tori_80.png",
+        reverse: "./torifuda/tori_r_80.png",
+        order: 79
+    },
+    {
+        no: 81,
+        kimariji: "ほ",
+        normal: "./torifuda/tori_81.png",
+        reverse: "./torifuda/tori_r_81.png",
+        order: 6
+    },
+    {
+        no: 82,
+        kimariji: "おも",
+        normal: "./torifuda/tori_82.png",
+        reverse: "./torifuda/tori_r_82.png",
+        order: 65
+    },
+    {
+        no: 83,
+        kimariji: "よのなかよ",
+        normal: "./torifuda/tori_83.png",
+        reverse: "./torifuda/tori_r_83.png",
+        order: 41
+    },
+    {
+        no: 84,
+        kimariji: "ながら",
+        normal: "./torifuda/tori_84.png",
+        reverse: "./torifuda/tori_r_84.png",
+        order: 78
+    },
+    {
+        no: 85,
+        kimariji: "よも",
+        normal: "./torifuda/tori_85.png",
+        reverse: "./torifuda/tori_r_85.png",
+        order: 39
+    },
+    {
+        no: 86,
+        kimariji: "なげけ",
+        normal: "./torifuda/tori_86.png",
+        reverse: "./torifuda/tori_r_86.png",
+        order: 81
+    },
+    {
+        no: 87,
+        kimariji: "む",
+        normal: "./torifuda/tori_87.png",
+        reverse: "./torifuda/tori_r_87.png",
+        order: 1
+    },
+    {
+        no: 88,
+        kimariji: "なにわえ",
+        normal: "./torifuda/tori_88.png",
+        reverse: "./torifuda/tori_r_88.png",
+        order: 84
+    },
+    {
+        no: 89,
+        kimariji: "たま",
+        normal: "./torifuda/tori_89.png",
+        reverse: "./torifuda/tori_r_89.png",
+        order: 54
+    },
+    {
+        no: 90,
+        kimariji: "みせ",
+        normal: "./torifuda/tori_90.png",
+        reverse: "./torifuda/tori_r_90.png",
+        order: 47
+    },
+    {
+        no: 91,
+        kimariji: "きり",
+        normal: "./torifuda/tori_91.png",
+        reverse: "./torifuda/tori_r_91.png",
+        order: 27
+    },
+    {
+        no: 92,
+        kimariji: "わがそ",
+        normal: "./torifuda/tori_92.png",
+        reverse: "./torifuda/tori_r_92.png",
+        order: 72
+    },
+    {
+        no: 93,
+        kimariji: "よのなかは",
+        normal: "./torifuda/tori_93.png",
+        reverse: "./torifuda/tori_r_93.png",
+        order: 40
+    },
+    {
+        no: 94,
+        kimariji: "みよ",
+        normal: "./torifuda/tori_94.png",
+        reverse: "./torifuda/tori_r_94.png",
+        order: 48
+    },
+    {
+        no: 95,
+        kimariji: "おおけ",
+        normal: "./torifuda/tori_95.png",
+        reverse: "./torifuda/tori_r_95.png",
+        order: 68
+    },
+    {
+        no: 96,
+        kimariji: "はなさ",
+        normal: "./torifuda/tori_96.png",
+        reverse: "./torifuda/tori_r_96.png",
+        order: 32
+    },
+    {
+        no: 97,
+        kimariji: "こぬ",
+        normal: "./torifuda/tori_97.png",
+        reverse: "./torifuda/tori_r_97.png",
+        order: 60
+    },
+    {
+        no: 98,
+        kimariji: "かぜそ",
+        normal: "./torifuda/tori_98.png",
+        reverse: "./torifuda/tori_r_98.png",
+        order: 45
+    },
+    {
+        no: 99,
+        kimariji: "ひとも",
+        normal: "./torifuda/tori_99.png",
+        reverse: "./torifuda/tori_r_99.png",
+        order: 26
+    },
+    {
+        no: 100,
+        kimariji: "もも",
+        normal: "./torifuda/tori_100.png",
+        reverse: "./torifuda/tori_r_100.png",
+        order: 15
+    },
+];
